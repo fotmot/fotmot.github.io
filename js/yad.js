af488d181aabb1bff75d3ceb89fd2bc5529bc751
fotmot <fotmot@x-noname.ru>
2018-08-02 23:37:02 +0300

    add email

--- js/yad.js
+++ js/yad.js
@@ -11,9 +11,7 @@ function handleError(jqXHR, resp) {
     if (jqXHR.status === 403) {
         window.location = 'autorize.html'; // redirect page
     } else if (jqXHR.status === 404) {
-        alert("Указанного ресурса не существует");
-        $("#folder").click();
-        return;
+        console.error("Unknown error: ", jqXHR);
     } else {
         console.error("Unknown error: ", jqXHR);
     }
