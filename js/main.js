fcce8067339250a6315a0820a30b53817a2c9fb0
Ivan Kazakov <i.kazakov@corp.badoo.com>
2019-04-24 11:59:10 +0300

    Template for future

--- js/main.js
+++ js/main.js
@@ -1,31 +1,34 @@
 $(function () {
+    let video = $("#video");
+    let container = $("#container");
+
     function playVideo(src) {
-        let video = $("#video");
         video.attr('src', src);
         video.attr('autoplay', true);
         video.attr('controls', true);
         video[0].load();
     }
 
-    function setPoster(src) {
-        let video = $("#video");
+    function showImage(src) {
         video[0].pause();
-        video.attr('autoplay', false);
         video.attr('controls', false);
+        video.attr('autoplay', false);
         video.attr('src', src);
         video.attr('poster', src);
         video[0].load();
-
     }
 
-    playVideo('//www.videvo.net/videvo_files/converted/2017_12/videos/171124_A1_HD_008.mp484461_jw.mp4');
-    setTimeout(function () {
-        setPoster('https://cdn.dribbble.com/users/66221/screenshots/1655593/html5.png');
+    //for starting
+    video.on('ended',function(){
+        video.off('ended');
+        playVideo('//www.videvo.net/videvo_files/converted/2017_12/videos/171124_A1_HD_008.mp484461_jw.mp4');
         setTimeout(function () {
-            playVideo('https://www.videvo.net/videvo_files/converted/2014_07/videos/Saint_Barthelemy.mov72328.mp4');
+            showImage('https://cdn.dribbble.com/users/66221/screenshots/1655593/html5.png');
+            setTimeout(function () {
+                playVideo('https://www.videvo.net/videvo_files/converted/2014_07/videos/Saint_Barthelemy.mov72328.mp4');
 
+            }, 10000);
         }, 10000);
-    }, 10000);
-
+    });
 
 });
