6f509852fd0c4f50a34677bb7a341134785725cf
Ivan Kazakov <i.kazakov@corp.badoo.com>
2019-04-26 09:55:02 +0300

    Yandex implemented v1

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
+const METHOD_YANDEX = 1;
+let video;
+let container;
+
+function playVideo(src, time) {
+    if (video == undefined || src == undefined || src.trim() == '') return;
+    clearTimeout(timer);
+    video.off('ended');
+    video.on('ended', function () {
+        video.off('ended');
+        startLoop();
+    });
+    video.attr('src', src);
+    video.attr('autoplay', true);
+    video.attr('controls', true);
+    video[0].load();
+}
+
+function showImage(src, time) {
+    if (video == undefined) return;
+    video.off('ended');
+    video[0].pause();
+    video.attr('controls', false);
+    video.attr('autoplay', false);
+    video.attr('src', src);
+    video.attr('poster', src);
+    video[0].load();
+}
+
 $(function () {
-    let video = $("#video");
-    let container = $("#container");
-
-    function playVideo(src) {
-        video.attr('src', src);
-        video.attr('autoplay', true);
-        video.attr('controls', true);
-        video[0].load();
-    }
-
-    function showImage(src) {
-        video[0].pause();
-        video.attr('controls', false);
-        video.attr('autoplay', false);
-        video.attr('src', src);
-        video.attr('poster', src);
-        video[0].load();
-    }
-
+    video = $("#video");
+    container = $("#container");
     checkResources();
-
     //for starting
     video.on('ended', function () {
         video.off('ended');
@@ -30,15 +41,22 @@ $(function () {
         checkYandex();
     }
 
-    function startLoop() {
-        playVideo('//www.videvo.net/videvo_files/converted/2017_12/videos/171124_A1_HD_008.mp484461_jw.mp4');
-        setTimeout(function () {
-            showImage('https://cdn.dribbble.com/users/66221/screenshots/1655593/html5.png');
-            setTimeout(function () {
-                playVideo('https://www.videvo.net/videvo_files/converted/2014_07/videos/Saint_Barthelemy.mov72328.mp4');
+    let isPaused = false;
+    let interval = 30000;
+    let current_method = METHOD_YANDEX;
 
-            }, 10000);
-        }, 10000);
+    function startLoop() {
+        clearTimeout(timer);
+        if (!isPaused) {
+            switch (current_method) {
+                case METHOD_YANDEX: {
+                    showYandex();
+
+                    break;
+                }
+            }
+        }
+        timer = setTimeout(startLoop, interval);
     }
 
 });
--- js/yandex.js
+++ js/yandex.js
@@ -1,6 +1,27 @@
 const URL = 'https://cloud-api.yandex.net:443/v1';
+const YAD_METHOD_RANDOM = 1;
+const YAD_METHOD_LAST = 2;
+const YAD_AUTOLOAD_FOLDER = 'photostream';
+
 
 let yad = Cookies.get('yad');
+let yad_settings = localStorage['yad_settings'];
+if (yad_settings == undefined) {
+    yad_settings = JSON.stringify({
+        method: YAD_METHOD_RANDOM,
+        folder: YAD_AUTOLOAD_FOLDER,
+        total: {},
+        total_created: (new Date()).getTime()
+    });
+    localStorage['yad_settings'] = yad_settings;
+}
+yad_settings = JSON.parse(yad_settings);
+if (yad_settings.total_created + 86400000 < (new Date()).getTime()) {
+    //сбрасываем кеш
+    yad_settings.total = {};
+    yad_settings.total_created = (new Date()).getTime();
+    localStorage.yad_settings = JSON.stringify(yad_settings);
+}
 
 function setHeader(xhr) {
     xhr.setRequestHeader('Authorization', 'OAuth ' + yad);
@@ -18,19 +39,18 @@ function handleError(jqXHR, resp) {
 }
 
 function checkYandex() {
-    if (false && yad == undefined) {
+    if (yad == undefined) {
         autorize();
     } else {
         getDisk({fields: 'user'}, function () {
             console.log('Autorization is fine');
             return;
         }, function (jqXHR, resp) {
-            if (jqXHR.status == 401) {
+            if (jqXHR.status == 401 || jqXHR.status == 401) {
                 autorize();
             } else {
                 console.log(jqXHR)
             }
-
         });
     }
 }
@@ -43,6 +63,10 @@ function getDisk(params, good, bad) {
     q('GET', URL + '/disk', params, bad).done(good);
 }
 
+function getResources(params, good, bad) {
+    q('GET', URL + '/disk/resources', params, bad).done(good);
+}
+
 function q(type, url, params, bad) {
     return $.ajax({
         url: url,
@@ -53,4 +77,78 @@ function q(type, url, params, bad) {
         error: bad,
         beforeSend: setHeader
     });
+}
+
+function showYandex() {
+    if (yad_settings.method == YAD_METHOD_RANDOM) {
+        if (yad_settings.folder == YAD_AUTOLOAD_FOLDER) { //узнаем настоящее имя папки
+            getDisk('system_folders.photostream', function (response) {
+                console.log('Folder is:', response.system_folders.photostream);
+                yad_settings.folder = response.system_folders.photostream;
+                localStorage.yad_settings = JSON.stringify(yad_settings);
+            }, function (jqXHR, resp) {
+                if (jqXHR.status == 401 || jqXHR.status == 401) {
+                    autorize();
+                } else {
+                    chooseFolder();
+                }
+            })
+        } else {
+            //check folder exist
+            console.log(localStorage);
+            loadFolder();
+        }
+    }
+}
+
+function loadFolder(folder = yad_settings.folder) {
+    if (yad_settings['total'][folder] == undefined) {
+        console.log('LOAF FOLDER: ', folder);
+        getResources({path: folder}, function (response) {
+            let total = response['_embedded']['total'];
+            if (total < 1 && folder != yad_settings.folder) {
+                return false; //need chose another folder
+            } else if (total < 1) {
+                chooseFolder();
+            } else {
+                yad_settings['total'][folder] = total;
+                localStorage.yad_settings = JSON.stringify(yad_settings);
+                loadResource(folder);
+            }
+        }, function () {
+            if (jqXHR.status == 401 || jqXHR.status == 401) {
+                autorize();
+            } else {
+                chooseFolder();
+            }
+        })
+    } else {
+        loadResource(folder);
+    }
+}
+
+function loadResource(folder) {
+    let offset = random(0, yad_settings.total[folder]);
+    getResources({path: folder, limit: 1, offset: offset}, function (response) {
+            let item = response._embedded.items[0];
+            if (item.type == 'file') {
+                if (item.media_type == 'image') {
+                    showImage(item.file, item.exif.date_time);
+                } else if (item.media_type == 'video') {
+                    playVideo(item.file, item.exif.date_time);
+                } else {
+                    console.log('Not image and video', item);
+                    loadResource();
+                }
+            } else if (item.type == 'dir') {
+                loadFolder(item.path)
+            }
+        }
+        , handleError);
+}
+
+function chooseFolder() {
+    console.log('Let choose folder');
+    //clear rnd collector
+    rnd = [];
 }
