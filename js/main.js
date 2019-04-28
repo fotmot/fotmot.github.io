a30f096554fd56f2d6f2ce46f2a92019e76af7ee
fotmot <fotmot@x-noname.ru>
2019-04-29 00:36:58 +0300

    Let video will play until it ending

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-const METHOD_YANDEX = 1;
-const METHOD_INTERNET = 2;
+const METHOD_INTERNET = '1';
+const METHOD_YANDEX = '2';
+
 let video;
 let overlay;
 let container;
 let startLoop;
 
+let current_method = localStorage.method;
+if(current_method == undefined){
+    current_method = METHOD_INTERNET;
+}
+
 function playVideo(src, html = '') {
     if (video == undefined || src == undefined || src.trim() == '') return;
     clearTimeout(timer);
     overlay.html(html);
+    video.css({background: 'black'});
+
     video.off('ended');
     video.on('ended', function () {
         video.off('ended');
@@ -23,19 +31,62 @@ function playVideo(src, html = '') {
 function showImage(src, html = '') {
     if (video == undefined) return;
     overlay.html(html);
+    video.css({background: 'transparent url("'+src+'") 50% 50% / cover no-repeat'});
+
     video.off('ended');
     video[0].pause();
     video.attr('controls', false);
     video.attr('autoplay', false);
     video.attr('src', src);
-    video.attr('poster', src);
+    video.attr('poster', "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7");
     video[0].load();
+}
+
+function toggleSettings(){
+    let settings = $('#settings');
+    let sbut = $('#sbut');
+
+    if(settings.is(':hidden')){
+        settings.show();
+        settings.animate({
+            left: "+=105%"
+        }, 1000, function() {
+            sbut.html('&#10006;');
+            sbut.css({opacity:0.5});
+        });
+    }else{
+        settings.animate({
+            left: "-=105%"
+        }, 1000, function() {
+            settings.hide();
+            sbut.html('&#9776;');
+            sbut.css({opacity:1});
+        });
+    }
+
+}
+
+function changeMethod(method){
+    if(localStorage.method!=method){
+        localStorage.method = method;
+    }
+    current_method =method;
+    startLoop();
 }
 
 $(function () {
     video = $("#video");
-    overlay = $("#overlay");
+    overlay = $("#meta");
     container = $("#container");
+    $(document).dblclick(function () {
+        if (!document.webkitIsFullScreen) {
+            window.document.body.webkitRequestFullscreen();
+        } else {
+            window.document.webkitCancelFullScreen();
+        }
+    });
+
+    $('#sel_method').val(current_method);
     //for starting
     video.on('ended', function () {
         video.off('ended');
@@ -45,7 +96,7 @@ $(function () {
 
     let isPaused = false;
     let interval = 30000;
-    let current_method = METHOD_INTERNET;
+
 
     startLoop = function () {
         clearTimeout(timer);
