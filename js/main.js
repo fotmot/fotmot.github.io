dfcc4fd9b982e28d04277bbbdae6af6aa2a9f36f
Ivan Kazakov <i.kazakov@corp.badoo.com>
2019-04-29 11:24:42 +0300

    pixels.com implemented v1

--- js/main.js
+++ js/main.js
@@ -6,22 +6,35 @@ let overlay;
 let container;
 let startLoop;
 
+let buffer = [];
+
+
 let current_method = localStorage.method;
-if(current_method == undefined){
+if (current_method == undefined) {
     current_method = METHOD_INTERNET;
+}
+
+
+function setMeta(html, src) {
+    overlay.html(html);
+    let ov_width = overlay.width();
+    let sc_width = window.innerWidth;
+    let position = (sc_width - ov_width) / 2;
+    overlay.parent().css('left', position);
 }
 
 function playVideo(src, html = '') {
     if (video == undefined || src == undefined || src.trim() == '') return;
     clearTimeout(timer);
-    overlay.html(html);
+    setMeta(html);
     video.css({background: 'black'});
-
     video.off('ended');
     video.on('ended', function () {
         video.off('ended');
         startLoop();
     });
+    video.data('prev-type', 'video');
+    video.data('prev-src', src);
     video.attr('src', src);
     video.attr('autoplay', true);
     video.attr('controls', true);
@@ -29,55 +42,72 @@ function playVideo(src, html = '') {
 }
 
 function showImage(src, html = '') {
+    console.log('showImage');
     if (video == undefined) return;
-    overlay.html(html);
-    video.css({background: 'transparent url("'+src+'") 50% 50% / cover no-repeat'});
-
+    setMeta(html, src);
+    video.css({background: 'transparent url("' + src + '") 50% 50% / cover no-repeat'});
     video.off('ended');
     video[0].pause();
     video.attr('controls', false);
     video.attr('autoplay', false);
+    video.data('prev-type', 'image');
+    video.data('prev-src', src);
     video.attr('src', src);
     video.attr('poster', "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7");
     video[0].load();
 }
 
-function toggleSettings(){
+function toggleSettings() {
     let settings = $('#settings');
     let sbut = $('#sbut');
 
-    if(settings.is(':hidden')){
+    if (settings.is(':hidden')) {
         settings.show();
         settings.animate({
             left: "+=105%"
-        }, 1000, function() {
+        }, 1000, function () {
             sbut.html('&#10006;');
-            sbut.css({opacity:0.5});
+            sbut.css({opacity: 0.5});
         });
-    }else{
+    } else {
         settings.animate({
             left: "-=105%"
-        }, 1000, function() {
+        }, 1000, function () {
             settings.hide();
             sbut.html('&#9776;');
-            sbut.css({opacity:1});
+            sbut.css({opacity: 1});
         });
     }
 
 }
 
-function changeMethod(method){
-    if(localStorage.method!=method){
+function changeMethod(method) {
+    if (localStorage.method != method) {
         localStorage.method = method;
     }
-    current_method =method;
+    current_method = method;
     startLoop();
+}
+
+function setPrevNextBindings() {
+    $(document).click(function (event) {
+        let scw = window.innerWidth;
+        let clickX = event.offsetX;
+        if (clickX < scw / 4) {
+            startLoop(buffer.length > 1 ? buffer.pop() : undefined);
+        }
+        if (clickX > 3 * scw / 4) {
+            startLoop();
+        }
+    });
 }
 
 $(function () {
     video = $("#video");
     overlay = $("#meta");
     container = $("#container");
+    setMeta('← Выбрать источник');
+    setPrevNextBindings();
     $(document).dblclick(function () {
         if (!document.webkitIsFullScreen) {
             window.document.body.webkitRequestFullscreen();
@@ -98,18 +128,30 @@ $(function () {
     let interval = 30000;
 
 
-    startLoop = function () {
+    startLoop = function (srcObject) {
         clearTimeout(timer);
         if (!isPaused) {
-            switch (current_method) {
-                case METHOD_YANDEX: {
-                    checkYandex();
-                    showYandex();
-                    break;
+            if (srcObject != undefined) {
+                if (srcObject.type == 'video') {
+                    playVideo(srcObject.src, srcObject.html);
+                } else {
+                    showImage(srcObject.src, srcObject.html);
                 }
-                case METHOD_INTERNET: {
-                    showPixel();
-                    break;
+            } else {
+                buffer.push({src: video.data('prev-src'), html: overlay.html(), type: video.data('prev-type')});
+                if (buffer.length >= 1000) {
+                    buffer = buffer.slice(500, 999);
+                }
+                switch (current_method) {
+                    case METHOD_YANDEX: {
+                        checkYandex();
+                        showYandex();
+                        break;
+                    }
+                    case METHOD_INTERNET: {
+                        showPixel();
+                        break;
+                    }
                 }
             }
         }
